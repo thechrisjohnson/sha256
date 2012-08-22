8633fdcb1cf92cc85adcf36948ef9feaeb7564be
Christopher Johnson <cdj2981@gmail.com>
2012-08-22 02:07:25 -0400

    Updated license at tops of files.

--- src/sha256.h
+++ src/sha256.h
@@ -14,7 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- *along with sha256.  If not, see <http://www.gnu.org/licenses/>.
+ * along with sha256.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #define ROTR(x, n, w) ((x>>n) | (x<<(w-n)))
